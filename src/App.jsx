3b145d6476b1db237491fb2e95d98ade1a67493e
pallabpal-1998 <pallabpalsip@gmail.com>
2024-07-03 17:04:43 +0530

    add hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
      <Navbar />
+     <Hero />
     </>
   )
 }
